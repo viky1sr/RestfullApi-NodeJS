a8feb2f9a8b0a1ab6d1f2421e7a7855c5c698979
viky muhamad <vikymuhamad@gmail.com>
2020-08-15 00:46:41 +0700

    update and delet

--- controller/bootcampController.js
+++ controller/bootcampController.js
@@ -1,11 +1,41 @@
 const BootcampDB = require('../models/bootcamps');
 
-exports.getBootcamps = (req,res,next) => {
-    res.status(200).json({success: true , message: 'Show all data bootcamps'});
+exports.getBootcamps = async (req,res,next) => {
+    try {
+        const bootcamp = await BootcampDB.find();
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    }catch (e) {
+        res.status(400).json({
+            success: false,
+        })
+    }
 }
 
-exports.getBootcamp = (req,res,next) => {
-    res.status(200).json({success: true , message: `Show data bootcamps by id ${req.params.id}`});
+exports.getBootcamp = async (req,res,next) => {
+    try {
+        const bootcamp = await BootcampDB.findById(req.params.id);
+
+        if (!bootcamp) {
+            res.status(400).json({
+                success: false,
+                message: 'Bootcamp not found'
+            })
+        }
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+
+    }catch (e) {
+        res.status(400).json({
+            success: false,
+        })
+    }
 }
 
 exports.createBootcamp = async (req,res,next) => {
@@ -24,10 +54,48 @@ exports.createBootcamp = async (req,res,next) => {
 
 }
 
-exports.updateBootcamp = (req,res,next) => {
-    res.status(200).json({success: true , message: `Update data bootcamps by id ${req.params.id}`});
+exports.updateBootcamp = async (req,res,next) => {
+    try {
+        const bootcamp = await BootcampDB.findByIdAndUpdate(req.params.id,req.body);
+
+        if (!bootcamp) {
+            res.status(400).json({
+                success: false,
+                message: 'Bootcamp not found'
+            })
+        }
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+
+    }catch (e) {
+        res.status(400).json({
+            success: false,
+        })
+    }
 }
 
-exports.deleteBootcamp = (req,res,next) => {
-    res.status(200).json({success: true , message: `Delete data bootcamps by id ${req.params.id}`});
+exports.deleteBootcamp = async (req,res,next) => {
+    try {
+        const bootcamp = await BootcampDB.findByIdAndDelete(req.params.id,req.body);
+
+        if (!bootcamp) {
+            res.status(400).json({
+                success: false,
+                message: 'Bootcamp not found'
+            })
+        }
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+
+    }catch (e) {
+        res.status(400).json({
+            success: false,
+        })
+    }
 }
